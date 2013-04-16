9845a1ec8173576a0a454c03ec37b1b234dde5c4
martin stauber <martin.n.stauber@gmail.com>
2013-04-16 16:24:18 -0500

    Setting to use NodeJS IP/Port, last commit was bad!

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 #!/bin/env node
-var PORT = process.env.OPENSHIFT_NODEJS_IP || 8080;
-var IPADDRESS = process.env.OPENSHIFT_INTERNAL_IP || '127.0.0.1';
+var PORT = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var IPADDRESS = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 var express = require('express');
 var server;
