6fc35f42bb06cb70a25cbb189f1ad1f6cbca0d3e
mstauber <martin.n.stauber@gmail.com>
2013-04-16 23:50:54 -0500

    Adding simple chat and user stuff

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 #!/bin/env node
-var PORT = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var PORT = process.env.OPENSHIFT_NODEJS_PORT || 8000;
 var IPADDRESS = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 var express = require('express');
@@ -51,27 +51,39 @@ io.sockets.on('connection', function (socket) {
     //join a room
     socket.on('join', function(data) {
 
+        if (data.nick == '') {
+            data.nick = "Anonymous";
+        }
         var nickToUse = data.nick;
         var nickAddition = 1;
         var nickTaken = false;
         var canGrabControl = true;
+        var currentUsers = [];
+        var controllingUser = "";
+
+        nickToUse = nickToUse.substring(0, 20);
         //generate a good nick & check to see if someone has control, very quick-to-code solution
         do
         {
             nickTaken = false;
-            if (nickAddition != 1) {
-                nickToUse = data.nick + nickAddition;
-            }
+
             for (var i = 0; i < io.sockets.clients(String(data.room)).length; i++)
             { 
-                var otherNick = io.sockets.clients(String(data.room))[i].userobj.getNick();
-                var otherHasControl = io.sockets.clients(data.room)[i].userobj.getHasControl();
+
+                currentUsers[i] = io.sockets.clients(data.room)[i].userObj.getNick();
+                var otherNick = io.sockets.clients(String(data.room))[i].userObj.getNick();
+                var otherHasControl = io.sockets.clients(data.room)[i].userObj.getHasControl();
                 if (nickToUse == otherNick) {
                     nickTaken = true;
                 }
                 if (otherHasControl) {
                     canGrabControl = false;
+                    controllingUser = io.sockets.clients(data.room)[i].userObj.getNick();
                 }
+            }
+
+            if(nickTaken) {
+                nickToUse = data.nick + nickAddition;
             }
 
             nickAddition++;
@@ -79,9 +91,12 @@ io.sockets.on('connection', function (socket) {
         
         user = new User(String(data.room), nickToUse);
 
-        socket.userobj = user;
+        socket.userObj = user;
         
-        user.hasControl = canGrabControl;
+        if (io.sockets.clients(data.room).length == 0) {
+            user.hasControl = true;
+            controllingUser = user.getNick();
+        }
         socket.join(data.room);
         
 
@@ -90,33 +105,93 @@ io.sockets.on('connection', function (socket) {
             nick: nickToUse
         });
 
-        socket.emit('control', {hasControl: canGrabControl, atConnection: true});
+        socket.emit('join', {hasControl: user.hasControl, users: currentUsers, controller: controllingUser, realNick: nickToUse });
     });
+
+
+
+
+
+
+
+
+
     
     //forward change events
     socket.on('change', function(data) {
-        console.log(data);
         socket.broadcast.to(user.room).emit('change', data);
 
     });
 
+
+
+
+
+
+
+
+    //on control request
+    socket.on('controlRequest', function(data) {
+
+        var controllingClient;
+        var controlIsOpen = true;
+
+        for (var i=0; i < io.sockets.clients(socket.userObj.getRoom()).Length; i++) {
+            if(io.sockets.clients(socket.userObj.getRoom())[i].userObj.getHasControl()) {
+                controlIsOpen = false;
+                controllingClient = io.sockets.clients(socket.userObj.getRoom())[i];
+            }
+        }
+
+        if (controlIsOpen) {
+            io.sockets.in(socket.userObj.getRoom()).emit('control-taken', {
+                nick: socket.userObj.getNick()
+            });
+        } else {
+            io.sockets.clients(socket.userObj.getRoom())[i].emit('control-requested', {
+                nick: socket.userObj.getNick()
+            });
+        }
+
+    });
+
+
+
+
+
+
+
+
+
+
+
+
     //simple chat
     socket.on('chat', function(data) {
-        socket.broadcast.to(String(user.room)).emit('msg', {
-            nick: socket.userobj.nick,
-            msg: data.msg
+        io.sockets.in(String(socket.userObj.getRoom())).emit('msg', {
+            nick: socket.userObj.getNick(),
+            msg: data
         });
     });
+
+
+
+
+
+
+
+
+
+
     
     // Set up listeners on the server side.
     socket.once('disconnect', function() {
-        // Respond if the client side voluntarily disconnects, but respond
-        // only once. It appears that disconnecting will fire more disconnect
-        // messages, whether from the client or server. So respond once and
-        // only once for each client.
-    //    socket.broadcast.to(user.room).emit('user-left', {
-     //       nick: user.nick
-      //  });
-      //  disconnectSocket();
+
+        //notify everyone a user left and whether they had control or not
+       io.sockets.in(user.getRoom()).emit('user-left', {
+            nick: user.getNick(),
+            controlOpen: user.getHasControl()
+        });
+
     });
 });
