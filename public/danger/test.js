7df422eef6dc20601dd95e7470230d6870f14c9f
Martin <martin.n.stauber@gmail.com>
2013-07-26 20:18:39 -0500

    Edit

--- public/danger/test.js
+++ public/danger/test.js
@@ -1,2 +1,3 @@
 
-$('body').css('background-image', 'url(http://www.wallpaper4me.com/images/wallpapers/bluematrixcode(3)-268120.jpeg)');
+$('body').css('background', '#f9c244 url(http://images1.wikia.nocookie.net/__cb31/dragonball/images/5/50/Wiki-background) top repeat ');
+$("#kristyfish").attr('src', 'http://images5.fanpop.com/image/photos/27000000/Goku-goku-27072376-232-276.png');
