7253a9fa6190950296a4094bba4dec568bb0f5b4
Martin Stauber <martin.n.stauber@gmail.com>
2014-02-21 10:23:35 -0700

    Cleanup
    removed some random floating files

--- test.js
+++
@@ -1,5 +0,0 @@
-console.log(Date.now());
-console.log(new Date().getTimezoneOffset());
-console.log(new Date().getTime());
-var d = new Date();
-console.log(d.getTime() + (d.getTimezoneOffset() * 60000));
